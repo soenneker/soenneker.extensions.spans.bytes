7c055eb83b3c319147684e2593d5b8642ba69fc1
Jake Soenneker <jake@soenneker.com>
2025-12-02 09:58:05 -0600

    Initial commit

---
+++ test/Soenneker.Extensions.Spans.Bytes.Tests/SpanByteExtensionTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.Spans.Bytes.Tests;
+
+public sealed class SpanByteExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
